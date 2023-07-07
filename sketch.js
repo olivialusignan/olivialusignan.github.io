695b010de47d575453191a7806f7f41a1b51a1eb
olivialusignan <138821949+olivialusignan@users.noreply.github.com>
2023-07-07 08:57:49 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,33 @@
 //Olivia Lusignan- Car
 
+let sun
+let rotation = 0
+
+let x = 0
+let carGo = false
+let carStop = false
+
+function preload(){
+  sun= loadImage('sun.png')
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-   print(windowWidth)
+  print(windowWidth)
+  imageMode(CENTER)
+  angleMode(DEGREES)
 }
 
 function draw() {
+
+
+  if(carGo == true){
+    x = x + 1
+  }
+  if(carStop == true){
+    x = x + 0
+  }
+
   //blue background
   background(51, 133, 255)
 
@@ -26,40 +48,65 @@ function draw() {
   fill(51, 133, 2550)
   rect(windowWidth/2, 150, windowWidth, 450)
 
+  
+rotation++
+
+push()
+rotate(radians(rotation))
+image(sun, 0, 0)
+pop()
+  
+
 }
 
-
-  
   //car base
   
   noStroke()
   fill(255,0,0)
   rectMode(CENTER)
-  rect(windowWidth/2, windowHeight/2, 400, 110)
+  rect(windowWidth/2 + x, windowHeight/2, 400, 110)
   
   //car head
-  rect(windowWidth/2, 245, 175, 100)
+  rect(windowWidth/2 + x, 245, 175, 100)
   
   //left and right of car head
-  triangle(550, 300, 600, 194, 650, 300)
-  triangle(720, 300, 770, 194, 820, 300)
+  triangle(550 + x, 300, 600 + x, 194, 650 + x, 300)
+  triangle(720 + x, 300, 770 + x, 194, 820 + x, 300)
   
   //car windows
   fill(255, 255, 255)
-  rect(645, 245, 70, 70)
-  rect(735, 245, 70, 70)
-  triangle(580, 280, 610, 210, 660, 280)
+  rect(645 + x, 245, 70, 70)
+  rect(735 + x, 245, 70, 70)
+  triangle(580 + x, 280, 610 + x, 210, 660 + x, 280)
  
   //car tires
   fill(0)
-  ellipse(600, 400, 95, 95)
-  ellipse(770, 400, 95, 95)
+  ellipse(600 + x, 400, 95, 95)
+  ellipse(770 + x, 400, 95, 95)
   
   fill(255, 255, 255)
-  ellipse(600, 400, 55, 55)
-  ellipse(770, 400, 55, 55)
+  ellipse(600 + x, 400, 55, 55)
+  ellipse(770 + x, 400, 55, 55)
+
+  textSize(40);
+  text('If you want to go, press g', 500, 50);
+  text('If you want to stop, press s', 500, 100);
 
 
+}
+
+
+function keyPressed(){
+  if(key == 'g'){
+    carGo = true 
+    carStop = false
+
+  }
+
+  if(key == 's'){
+    carGo = false 
+    carStop = true
+}
 }
 
 function windowResized(){
